214130c457a07beeed5adb474065c13757a41c70
Alexnder Boychenko <alexander.boychenko@bimeister.com>
2022-12-06 22:14:28 +0300

    fix

--- Version_2/CRM/UserService.cs
+++ Version_2/CRM/UserService.cs
@@ -24,16 +24,3 @@ public class UserService
     }
 }
 
-public interface IMessageBus
-{
-    public void SendEmailChangedMessage(int userId, string newEmail);
-}
-
-internal class MessageBus : IMessageBus
-{
-    public void SendEmailChangedMessage(int userId, string newEmail)
-    {
-        Console.WriteLine("Email has been sent");
-    }
-}
-
