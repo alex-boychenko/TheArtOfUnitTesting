9097517f6c55d417ba33a2c0db65af87059fcbf4
Alexnder Boychenko <alexander.boychenko@bimeister.com>
2022-12-06 22:18:08 +0300

    remove redundant implementations

--- Version_2/CRM/Database.cs
+++ Version_2/CRM/Database.cs
@@ -7,26 +7,3 @@ public interface IDatabase
     public void SaveCompany(Company company);
     public void SaveUser(User user);
 }
-
-internal class Database : IDatabase
-{
-    public Company GetCompany()
-    {
-        return new Company(10, "some.domain");
-    }
-
-    public User GetUserById(int userId)
-    {
-        return new User(userId, "some.email", UserType.Employee);
-    }
-
-    public void SaveCompany(Company company)
-    {
-        Console.WriteLine("Company vas saved");
-    }
-
-    public void SaveUser(User user)
-    {
-        Console.WriteLine("User vas saved");
-    }
-}
--- Version_2/CRM/MessageBus.cs
+++ Version_2/CRM/MessageBus.cs
@@ -4,11 +4,3 @@ public interface IMessageBus
 {
     public void SendEmailChangedMessage(int userId, string newEmail);
 }
-
-internal class MessageBus : IMessageBus
-{
-    public void SendEmailChangedMessage(int userId, string newEmail)
-    {
-        Console.WriteLine("Email has been sent");
-    }
-}
